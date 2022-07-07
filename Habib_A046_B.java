7604c0e24fe23e627f4f1074d3321b4c5b119b2b
Mathabib <91876177+Mathabib@users.noreply.github.com>
2022-07-07 22:46:08 +0700

    Add files via upload

---
+++ Habib_A046_B.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package habib_a046_b;
+
+/**
+ *
+ * @author Ahmad Habib
+ */
+public class Habib_A046_B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
